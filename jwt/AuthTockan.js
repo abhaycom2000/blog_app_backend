162288166854126310e6b696f4bc99efa283525f
abhay <abhaycom2000>
2025-07-01 16:31:41 +0530

    Changes by auth permission

--- jwt/AuthTockan.js
+++ jwt/AuthTockan.js
@@ -6,9 +6,9 @@ const createTokenAndSaveCookies = async (userId, res) => {
         expiresIn: '30d'
     })
     res.cookie("jwt", token, {
-        httpOnly: true,
+        httpOnly: false,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         //  maxAge: 30 * 24 * 60 * 60 * 1000,
     })
 
